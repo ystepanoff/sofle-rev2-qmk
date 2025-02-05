0e511515ec5c6eb4c7b34e6b8cb4d9355e35a4a6
Yegor Stepanov <ystepanoff@icloud.com>
2025-02-05 01:54:58 +0000

    First experiments with the firmware

---
+++ keymaps/default/config.h
@@ -0,0 +1,9 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
+#define OLED_TIMEOUT 0
---
+++ sofle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifdef KEYBOARD_sofle_rev1
+    #include "rev1.h"
+#endif
+#ifdef KEYBOARD_sofle_rev2
+    #include "rev2.h"
+#endif
